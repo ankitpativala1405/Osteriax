1bcea31de8b7b318a3fa58235b1a990791552566
Your Name <ankitj1405@gmail.com>
2025-07-13 15:38:29 +0530

    update || 13-07

--- Front-end/src/components/Testimonial.jsx
+++ Front-end/src/components/Testimonial.jsx
@@ -14,40 +14,7 @@ const Testimonial = () => {
                     <h2>What Customers Say</h2>
                   </div>
                   <div className="mu-testimonial-content">
-
                     <ul className="mu-testimonial-slider">
-                      <li>
-                        <div className="mu-testimonial-single">
-                          <div className="mu-testimonial-info">
-                            <p>
-                              Lorem ipsum dolor sit amet, consectetur adipisicing
-                              elit. Cupiditate consequuntur ducimus cumque iure modi
-                              nesciunt recusandae eligendi vitae voluptatibus,
-                              voluptatum tempore, ipsum nisi perspiciatis. Rerum
-                              nesciunt fuga ab natus, dolorem?
-                            </p>
-                          </div>
-                          <div className="mu-testimonial-bio">
-                            <p>- David Muller</p>
-                          </div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className="mu-testimonial-single">
-                          <div className="mu-testimonial-info">
-                            <p>
-                              Lorem ipsum dolor sit amet, consectetur adipisicing
-                              elit. Cupiditate consequuntur ducimus cumque iure modi
-                              nesciunt recusandae eligendi vitae voluptatibus,
-                              voluptatum tempore, ipsum nisi perspiciatis. Rerum
-                              nesciunt fuga ab natus, dolorem?
-                            </p>
-                          </div>
-                          <div className="mu-testimonial-bio">
-                            <p>- David Muller</p>
-                          </div>
-                        </div>
-                      </li>
                       <li>
                         <div className="mu-testimonial-single">
                           <div className="mu-testimonial-info">
