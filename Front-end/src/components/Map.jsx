ee1eb881b4003f57492cccb89dc8e5df7b12e1af
Your Name <ankitj1405@gmail.com>
2025-07-13 13:55:45 +0530

    update || 13-07

--- Front-end/src/components/Map.jsx
+++ Front-end/src/components/Map.jsx
@@ -5,7 +5,7 @@ const Map = () => {
     <>
       <section id="mu-map">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9207.358598888495!2d-85.64847801496286!3d30.183918972289003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x2320479d70eb6202!2sDillard's!5e0!3m2!1sbn!2sbd!4v1462359735720"
+         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d232.43727192324857!2d72.8212427!3d21.2319933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04fd1d8508c75%3A0x992886c7e5f89393!2sRed%20%26%20White%20Skill%20Education!5e0!3m2!1sen!2sin!4v1752394980535!5m2!1sen!2sin"
           width="100%"
           height="100%"
           frameBorder={0}
